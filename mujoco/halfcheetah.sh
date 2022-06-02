7f8090ed2673a4cd222e0fe4e0d54af6abaa75f4
Emiyali <Emiyalzn@gmail.com>
2022-06-02 23:47:09 +0800

    git ignore

--- mujoco/halfcheetah.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=1 python train.py --env_name HalfCheetah-v2 --seed 42 --method ppo --n_step 3000000
-CUDA_VISIBLE_DEVICES=1 python train.py --env_name HalfCheetah-v2 --seed 42 --method sac --n_step 3000000
-CUDA_VISIBLE_DEVICES=1 python train.py --env_name HalfCheetah-v2 --seed 42 --method sac --is_per True --n_step 3000000
--- mujoco/hopper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES=2 python train.py --env_name Hopper-v2 --seed 42 --method ppo --n_step 1000000
-CUDA_VISIBLE_DEVICES=2 python train.py --env_name Hopper-v2 --seed 42 --method sac --n_step 1000000
-CUDA_VISIBLE_DEVICES=2 python train.py --env_name Hopper-v2 --seed 42 --method sac --is_per True --n_step 1000000
